e6a8ed86c07503beb6ca59dd1a8709035500c77a
Antoine Joulie <antoine.joulie@gmail.com>
2014-04-28 15:49:26 +0200

    Add method Housetrip::Compression#encode
    Do not check if result is shoter than entry for now

--- lib/housetrip/compression.rb
+++ lib/housetrip/compression.rb
@@ -1,4 +1,9 @@
 module Housetrip
   module Compression
+    def self.encode(str)
+      str.scan(/(.)(\1*)/).map do |char, count|
+        [char, count.length + 1]
+      end.join
+    end
   end
 end
--- spec/housetrip/compression_spec.rb
+++ spec/housetrip/compression_spec.rb
@@ -1,6 +1,15 @@
 require 'housetrip/compression'
 
 describe Housetrip::Compression do
-  it 'loads' do
+  describe '#encode' do
+    it 'transform "aa" to "a2"' do
+      Housetrip::Compression.encode('aa').should eq('a2')
+    end
+    it 'transform "aaa" to "a3"' do
+      Housetrip::Compression.encode('aaa').should eq('a3')
+    end
+    it 'transform "aaabbbbbbbbbbccccc" to "a3b6c5"' do
+      Housetrip::Compression.encode('aaabbbbbbbbbbccccc').should eq('a3b10c5')
+    end
   end
 end
